275c94c8483090889b41db897375402c3d9cf89c
blasted-tiara <sectioaurea13@gmail.com>
2024-01-19 14:51:52 +0100

    Removed a "small random offset" when generating slices which introduced weird mesh artifacts

--- Assets/Scripts/TreeData/TreeSlice.cs
+++ Assets/Scripts/TreeData/TreeSlice.cs
@@ -4,19 +4,17 @@ public class TreeSlice {
     public static Vector3[] GenerateSlice(TreeNode center, int n) {
         int numberOfChildren = center.children == null ? 0 : center.children.Length;
 
-        Vector3[] vertices = new Vector3[n * numberOfChildren];
+        Vector3[] vertices = new Vector3[(n + 1) * numberOfChildren];
         
-        float smallRandomOffset = Random.Range(-0.5f, 0.5f);
-
         for (int childIndex = 0; childIndex < numberOfChildren; childIndex++) {
             float radius = numberOfChildren > 1 ? (center.radius + center.children[childIndex].radius) / 2f : center.radius;
 
             Vector3 normal = GetRingNormal(center, childIndex);
             float angleIncrement = 360f / n;
             Vector3 InitialVertex = Vector3.Normalize(Vector3.Cross(normal, Vector3.forward) ) * radius;
-            for (int vertexIndex = 0; vertexIndex < n; vertexIndex++) {
+            for (int vertexIndex = 0; vertexIndex <= n; vertexIndex++) {
                 if (normal != Vector3.zero) {
-                    vertices[childIndex * n + vertexIndex] = center.position + Quaternion.AngleAxis(smallRandomOffset + angleIncrement * vertexIndex, normal) * InitialVertex;
+                    vertices[childIndex * n + vertexIndex] = center.position + Quaternion.AngleAxis(angleIncrement * vertexIndex, normal) * InitialVertex;
                 } else {
                     vertices[vertexIndex] = center.position;
                 }
